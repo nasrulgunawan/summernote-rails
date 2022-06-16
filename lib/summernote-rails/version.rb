9a4e669ae6c4f50f7d08cd6939cdff719d135c9d
Arnaud Levy <contact@arnaudlevy.com>
2022-06-16 18:37:12 +0200

    nokogiri

--- lib/summernote-rails/version.rb
+++ lib/summernote-rails/version.rb
@@ -2,6 +2,6 @@ module SummernoteRails
   module Rails
     # The version of summernote-rails (X.Y.Z) is matched with that of original summernote editor.
     # Ex: 0.8.20.1 is matching with Summernote 0.8.20
-    VERSION = "0.8.20.5"
+    VERSION = "0.8.20.6"
   end
 end
