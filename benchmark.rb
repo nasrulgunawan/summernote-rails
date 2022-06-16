1bb96a566657715d34e7d447ec62bc12317d0804
Arnaud Levy <contact@arnaudlevy.com>
2022-06-16 11:45:42 +0200

    bench

---
+++ benchmark.rb
@@ -0,0 +1,11 @@
+require "benchmark/memory"
+require "summernote-rails"
+require "summernote-rails/cleaner"
+
+def clean
+  SummernoteCleaner.clean '<p>'
+end
+
+Benchmark.memory do |x|
+  x.report("clean") { clean }
+end
