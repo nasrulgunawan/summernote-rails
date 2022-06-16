dc6546cfc1cfbec75a338fffce4b37307636a4c8
Sébastien Gaya <sebastien.gaya@gmail.com>
2022-06-16 18:33:53 +0200

    nokogiri only

--- lib/summernote-rails/cleaner.rb
+++ lib/summernote-rails/cleaner.rb
@@ -25,62 +25,7 @@ class SummernoteCleaner
     @code = code.gsub("<p>\n</p>", "")
                 .gsub("<p></p>", "")
                 .gsub("<p><br></p>", "")
-    @max_loops = @code.length * 2
-    @current_loop = 0
-    @clean_code = ''
-    @current_block_tag = nil
-    @open_block_tag_found = nil
-    @close_block_tag_found = nil
-    @open_tags = {}
-    @close_tags = {}
   end
-
-  # def clean
-  #   while @code.length > 0 && @max_loops > @current_loop do
-  #     if starts_with_open_block_tag?
-  #       log "starts_with_open_block_tag #{@open_block_tag_found}"
-  #       unless @current_block_tag.nil?
-  #         log "Opening a block in a block, we need to close the previous one"
-  #         @clean_code.concat close(@current_block_tag)
-  #       end
-  #       log "Move the open block from code to clean code"
-  #       transfer open_current.length
-  #       @current_block_tag = @open_block_tag_found
-  #       @open_block_tag_found = nil
-  #     elsif starts_with_close_block_tag?
-  #       log "starts_with_close_block_tag #{@close_block_tag_found}"
-  #       if @close_block_tag_found == @current_block_tag
-  #         log "Everything is logical, we close what was opened"
-  #         transfer close_current.length
-  #       elsif @current_block_tag == nil
-  #         log "Nothing opened, just remove it"
-  #         remove close(@close_block_tag_found).length
-  #       else
-  #         log "Mismatch, the closing tag is not what it should be. We need to remove it, and add the correct one instead"
-  #         remove close(@close_block_tag_found).length
-  #         @clean_code.concat close_current
-  #       end
-  #       @current_block_tag = nil
-  #       @close_block_tag_found = nil
-  #     else
-  #       if in_block?
-  #         transfer 1
-  #       else
-  #         log "not in a block, we open a p"
-  #         @current_block_tag = DEFAULT_BLOCK
-  #         @clean_code.concat open_current
-  #       end
-  #     end
-  #     log @clean_code
-  #     @current_loop += 1
-  #   end
-  #   if in_block?
-  #     log "still in a block, we close with a #{@current_block_tag}"
-  #     @clean_code.concat close_current
-  #   end
-  #   log @clean_code
-  #   @clean_code
-  # end
 
   def clean
     fragment = Nokogiri::HTML5::DocumentFragment.parse(@code)
@@ -102,7 +47,7 @@ class SummernoteCleaner
 
     not_block_elements_collection.each do |nodes|
       first_node = nodes.first
-      new_paragraph = first_node.add_previous_sibling Nokogiri::XML::Node.new('p', fragment.document)
+      new_paragraph = first_node.add_previous_sibling Nokogiri::XML::Node.new(DEFAULT_BLOCK, fragment.document)
       nodes.each do |node|
         node.parent = new_paragraph
       end
@@ -112,68 +57,5 @@ class SummernoteCleaner
             .gsub("<p>\n</p>", "")
             .gsub("<p></p>", "")
             .gsub("<p><br></p>", "")
-  end
-
-  protected
-
-  def transfer(length)
-    add length
-    remove length
-  end
-
-  # 0..0 means first character
-  def add(length)
-    # Concat is much lighter than +=
-    # https://www.rubyguides.com/2019/07/ruby-string-concatenation/
-    @clean_code.concat @code[0..length-1].freeze
-  end
-
-  # 1..-1 means everything but the first
-  def remove(length)
-    @code = @code[length..-1].freeze
-  end
-
-  def starts_with_open_block_tag?
-    BLOCK_TAGS.each do |tag|
-      if @code.start_with? open(tag)
-        @open_block_tag_found = tag
-        return true
-      end
-    end
-    false
-  end
-
-  def starts_with_close_block_tag?
-    BLOCK_TAGS.each do |tag|
-      if @code.start_with? close(tag)
-        @close_block_tag_found = tag
-        return true
-      end
-    end
-    return false
-  end
-
-  def in_block?
-    !@current_block_tag.nil?
-  end
-
-  def open(tag)
-    @open_tags[tag] ||= "<#{tag}>".freeze
-  end
-
-  def close(tag)
-    @close_tags[tag] ||= "</#{tag}>".freeze
-  end
-
-  def open_current
-    open @current_block_tag
-  end
-
-  def close_current
-    close @current_block_tag
-  end
-
-  def log(string)
-    # puts string
   end
 end
